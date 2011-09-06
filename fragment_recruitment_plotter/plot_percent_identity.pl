4c05f4ef3535f7a227a55883fef4aa3e452f724d
David Wilkins <david@wilkox.org>
2011-09-06 16:15:05 +1000

    Tightening up plot

--- fragment_recruitment_plotter/plot_percent_identity.pl
+++ fragment_recruitment_plotter/plot_percent_identity.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 
-use warnings;
-
 #generate a percent identity scatterplot for blast hits on a full genome
 #written by david wilkins <david@wilkox.org>
 #lives at https://github.com/wilkox/blast-tools
@@ -22,7 +20,7 @@ unless ($Rversion =~ /R\sversion/) {
 $USAGE = q/USAGE:
 plot_percent_identity.pl 
         -r <filename> Reference genome in fasta format.
-        -b <filename> Blast output, in -m8 hit table format. You may plot multiple samples by using the -b flag multiple times, e.g. "-b firstsample.blast_output -b secondsample.blast_output". The percent identity maps for the different samples will be displayed on the sample plot, overlayed in different colours. A maximum of 5 different samples can be displayed on the same plot.
+        -b <filename> Blast output, in -m8 hit table format.
         -p <string> Prefix for output files.
         -i <number> Plot width, in pixels
         -h <number> Plot height, in pixels
@@ -32,15 +30,15 @@ plot_percent_identity.pl
 use Getopt::Long;
 GetOptions (
 'r=s' => \$reference_genome,
-'b=s' => \@blast_output,
+'b=s' => \$blast_output,
 'p=s' => \$output_prefix,
 'i=s' => \$plot_width,
 'h=s' => \$plot_height,
 ) or die $USAGE;
-die $USAGE if !$reference_genome or @blast_output == 0 or !$output_prefix or !$plot_height or !$plot_width;
-die ("ERROR - a maximum of 5 different samples can be displayed on the same plot") if @blast_output > 5;
+die $USAGE if !$reference_genome or !$blast_output or !$output_prefix or !$plot_height or !$plot_width;
 
 ##BODY
+my $maxID = 0;
 &get_length_of_reference_genome;
 &get_percent_identity;
 &draw_plot;
@@ -67,57 +65,35 @@ sub get_length_of_reference_genome {
 
 sub get_percent_identity {
 
-	#loop over multiple blast outputs
-	foreach $blast_output (@blast_output) {
+  die ("ERROR - could not open blast output $blast_output\n") unless open(BLAST, "<$blast_output");
+  print STDERR "\n";
 
-		die ("ERROR - could not open blast output $blast_output\n") unless open(BLAST, "<$blast_output");
-		print STDERR "\n";
+  while ($line = <BLAST>) {
+    print STDERR "\nProcessed $. lines of $blast_output..." if $. % 10000 == 0;
 
-		while ($line = <BLAST>) {
+    #get the start and end positions of the hit
+    chomp $line;
+    my @line = split(/\s+/, $line);
+    my @positions = ($line[8], $line[9]);
+    my @sorted = sort {$a <=> $b} (@positions);
+    my $startpos = $sorted[0];
+    my $endpos = $sorted[1];
+    my $percentidentity = $line[2];
+    $maxID = $maxID > $percentidentity ? $percentidentity : $maxID;
+    $read{$blast_output}{$line[0]}{'startpos'} = $startpos; 
+    $read{$blast_output}{$line[0]}{'endpos'} = $endpos;  
+    $read{$blast_output}{$line[0]}{'percentidentity'} = $percentidentity;  
+    
+    #make sure the start and end positions exist and are numbers
+    die ("ERROR - malformed line in blast output $blast_output at line $.\n") unless $startpos =~ /^\d+$/ && $endpos =~ /^\d+$/;
 
-			print STDERR "\nProcessed $. lines of $blast_output..." if $. % 10000 == 0;
+  }
 
-			#get the start and end positions of the hit
-			chomp $line;
-			my @line = split(/\s+/, $line);
-			my @positions = (@line[8], @line[9]);
-			my @sorted = sort {$a <=> $b} (@positions);
-			my $startpos = @sorted[0];
-			my $endpos = @sorted[1];
-			my $percentidentity = @line[2];
-      $maxID = $percentidentity unless $maxid >= $percentidentity;
-      $read{$blast_output}{$.}{'startpos'} = $startpos; 
-      $read{$blast_output}{$.}{'endpos'} = $endpos;  
-      $read{$blast_output}{$.}{'percentidentity'} = $percentidentity;  
-			
-			#make sure the start and end positions exist and are numbers
-			die ("ERROR - malformed line in blast output $blast_output at line $.\n") unless $startpos =~ /^\d+$/ && $endpos =~ /^\d+$/;
-
-		}
-
-		close BLAST;
-	}
+  close BLAST;
 }
 
 sub draw_plot {
 	
-	print STDERR "\nWriting values to temporary file plotfile.tmp for plotting...";
-
-	#each of multiple blast outputs gets a seperate plotfile
-	my $j = 0;
-	foreach $blast_output (@blast_output) {
-
-		die ("ERROR - could not create temporary plotfile plotfile.tmp\n") unless open(PLOT, ">$j-plotfile.tmp");
-		print PLOT "\"pos\",\"value\"";
-
-		for ($i = 1; $i <= $reference_genome_length; ++$i) { 
-      print PLOT "\n$i,$window_average{$blast_output}{$i}";
-		}
-
-		close PLOT;
-		++$j;
-	}
-
 	#generate R script
 	print STDERR "\nWriting R script to R.tmp...";
 	die ("ERROR - cannot create R script at R.tmp\n") unless open(R, ">R.tmp");
@@ -145,29 +121,15 @@ EOF
 	#R: draw a line for each read
 	my @rcolours = qw(#104E8B70 #B2222270 #228B2270 #8B0A5070 #CDAD0070);
 	$j = 0;
-  foreach my $blast_output (@blast_output) {
-    my $k = 1;
-    foreach my $readid (keys (%{$read{$blast_output}})) {
-      $script .= <<EOF;
-lines(c($read{$blast_output}{$readid}{'startpos'}, $read{$blast_output}{$readid}{'endpos'}), c($read{$blast_output}{$readid}{'percentidentity'}, $read{$blast_output}{$readid}{'percentidentity'}), col=c("@rcolours[$j]"))
+  my $k = 1;
+  foreach my $readid (keys (%{$read{$blast_output}})) {
+    $script .= <<EOF;
+lines(c($read{$blast_output}{$readid}{'startpos'}, $read{$blast_output}{$readid}{'endpos'}), c($read{$blast_output}{$readid}{'percentidentity'}, $read{$blast_output}{$readid}{'percentidentity'}), col=c("$rcolours[$j]"))
 EOF
-    }
-  ++$j;
   }
-#R: add a legend to the coverage plot my $legendText;
-	my $legendCols;
-	$j = 0;
-	foreach $blast_output (@blast_output) {
-		$legendText .= "\"$blast_output\",";
-		$legendCols .= "\"@rcolours[$j]\",";
-		++$j;
-	}
-	chop $legendText;
-	chop $legendCols;
-	$script .= <<EOF;
-legendtext = c($legendText)
-legendcols = c($legendCols)
-legend(c("topright"), legend=legendtext, fill=legendcols)
+
+  #close device
+  $script .= <<EOF;
 dev.off()
 EOF
 
